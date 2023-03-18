fdcb0bec8b4cabb02ff8d7d82af343c5cddda1dd
Graciotee <anitamay26@yahoo.com>
2023-03-18 07:09:03 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing a function f
+ *
+ * Return: 0 when done
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

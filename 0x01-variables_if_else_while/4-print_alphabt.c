1769e69ea6d1aa2248b3733e7d8ca72823ad518c
Graciotee <anitamay26@yahoo.com>
2023-03-20 13:33:23 -0700

    omit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - will always be my main
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}

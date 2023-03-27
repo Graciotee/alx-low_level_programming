58680eef0e00fae9335c36fdd4d1fde9db2d4270
Graciotee <anitamay26@gmail.com>
2023-03-27 10:20:04 -0700

    swap char

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap int - swap var values
+ * @a: pointera
+ * @b: pointerb
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}

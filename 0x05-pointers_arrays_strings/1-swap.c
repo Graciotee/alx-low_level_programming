00dee8847c5ca89a81eceaeb94aa25391b04a8cd
Graciotee <anitamay26@gmail.com>
2023-03-27 15:13:27 -0700

    swap intergers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - swap var values
- * @a: pointera
- * @b: pointerb
- * Return: void
+ * swap_int - swaps the value of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
 
 void swap_int(int *a, int *b)
 {
-	int a;
+	int m;
 
-	a = *a;
+	m = *a;
 	*a = *b;
-	*b = a;
+	*b = m;
 }

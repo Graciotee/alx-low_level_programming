53edea0be66f6461e046518fa5140fab409b8c56
Graciotee <anitamay26@gmail.com>
2023-03-27 18:10:51 -0700

    move strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - function that copies string pointed
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n - 0;
+	int m = 0;
+
+	while (*(src + n) != '\0')
+	{
+		n++;
+	}
+	for ( ; m < 1 ; m++)
+	{
+		dest[m] = src[m];
+	}
+	dest[n] = '\0';
+	return (dest);
+}
+
